261b180ead1e41ded36d38c4b3fd258f4144747d
Enmah4k <alexmm011@hotmail.com>
2020-02-17 21:26:37 -0400

    Empezando

---
+++ Ejercicio3/Controllers/DealerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_3.Controllers
+{
+    public class DealerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
